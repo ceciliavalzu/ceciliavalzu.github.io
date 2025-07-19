5bb5e9d828159b08fe8cd9dbb4d478b28ddb378c
ceciliavalzu <bcvalenciaz@gmial.com>
2025-07-19 14:47:57 -0500

    v1

---
+++ hola_mundo.py
@@ -0,0 +1,43 @@
+#print("Hola Ceci desde Python")
+
+# Este es un comentario de una solita líniecita
+
+
+""" 
+Este es un comentariote
+de 
+muchas 
+muchitas 
+muchotas lineas
+
+ """
+
+#Variables
+
+""" variable1 = 5
+variable2 = 8
+resSuma = 0
+resResta = 0
+resDivi = 0
+resMulti = 0
+
+resSuma = variable1 + variable2
+
+
+resResta = variable1 - variable2
+
+resMulti = variable1 * variable2
+
+resDivi = variable1 / variable2
+
+print("*"*50)
+print("El resultado de la suma es: " , resSuma)
+print("El resultado de la División es: " ,resDivi)
+print("El resultado de la Resta es: " ,resResta)
+print("El resultado de la Multiplicación es: " ,resMulti)
+print("*"*50) """
+
+n1 = int(input("Digita un numero: "))
+n2 = int(input("Digita otro numero: "))
+
+print("la suma es: " ,n1+n2)
